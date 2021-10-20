d10422289d3a4dd5358f01edda3cf9af509e4d7e
Diego Vicuña <diego704249@gmail.com>
2021-10-20 14:36:40 -0500

    Switch if else

---
+++ Condificonales.dart
@@ -0,0 +1,1 @@
+// es lo mismo q en c++, c, c#, js ,ts y java.
---
+++ SwitchCase.dart
@@ -0,0 +1,21 @@
+main(List<String> arguments) {
+  var opcion = 2;
+  switch (opcion) {
+    case 1:
+      {
+        print("Retornamos valor $opcion");
+      }
+      break;
+    case 2:
+      {
+        print("Retornamos valor $opcion");
+      }
+      break;
+
+    default:
+      {
+        print("opcion fuera del rango permitido");
+      }
+      break;
+  }
+}
